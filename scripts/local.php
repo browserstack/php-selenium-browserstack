d5a4acec5c76ea77dbe2873a63e3d626379b1230
RutvikChandla <rutvik.c@browserstack.com>
2022-04-08 09:41:37 +0530

    selenium 3 changes

---
+++ scripts/local.php
@@ -0,0 +1,44 @@
+<?php
+    require_once("vendor/autoload.php");
+    use Facebook\WebDriver\Remote\RemoteWebDriver;
+    use Facebook\WebDriver\WebDriverBy;
+    use Facebook\WebDriver\WebDriverExpectedCondition;
+    use BrowserStack\Local;
+
+    # Creates an instance of Local
+    $bs_local = new Local();
+  
+    # You can also set an environment variable - "BROWSERSTACK_ACCESS_KEY".
+    $bs_local_args = array("key" => "ACCESS_KEY");
+    # Starts the Local instance with the required arguments
+    $bs_local->start($bs_local_args);
+
+    # Check if BrowserStack local instance is running
+    echo $bs_local->isRunning();
+    $caps = array(
+        "browserName" => "iPhone",
+        "device" => "iPhone 11",
+        "realMobile" => "true",
+        "os_version" => "14.0",
+        "browserstack.local" => "true",
+        "name" => "BStack-[Php] Sample Test", // test name
+        "build" => "BStack Build Number 1" // CI/CD job or build name
+    );
+    $web_driver = RemoteWebDriver::create("https://USERNAME:ACCESS_KEY@hub-cloud.browserstack.com/wd/hub",$caps);
+    try{
+        $web_driver->get("http://bs-local.com:45691/check");
+        $body_text = $web_driver->wait(10000)->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector("body")))->getText();
+        # Setting the status of test as 'passed' or 'failed' based on the condition; if title of the web page starts with 'BrowserStack'
+        if ($body_text == "Up and running"){
+            $web_driver->executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "Local test ran successfully"}}' );
+        }  else {
+            $web_driver->executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Failed to load local test"}}');
+        }
+    }
+    catch(Exception $e){
+        echo 'Message: ' .$e->getMessage();
+    }
+    $web_driver->quit();
+      # Stop the Local instance
+    $bs_local->stop();
+?>
---
+++ scripts/single.php
@@ -0,0 +1,37 @@
+<?php
+require_once('vendor/autoload.php');
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverExpectedCondition;
+$caps = array(
+    "browserName" => "iPhone",
+    "device" => "iPhone 11",
+    "realMobile" => "true",
+    "os_version" => "14.0",
+    "name" => "BStack-[Php] Sample Test", // test name
+    "build" => "BStack Build Number 1" // CI/CD job or build name
+);
+$web_driver = RemoteWebDriver::create("https://USERNAME:ACCESS_KEY@hub-cloud.browserstack.com/wd/hub",$caps);
+try{
+    $web_driver->get("https://bstackdemo.com/");
+    $web_driver->wait(10000)->until(WebDriverExpectedCondition::titleIs("StackDemo"));
+    # getting text of the product
+    $product_on_page = $web_driver->wait(10000)->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::XPath("//*[@id='1']/p")))->getText();
+    # clicking the 'Add to cart' button
+    $web_driver->wait(10000)->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::XPath("//*[@id='1']/div[4]")))->click();
+    # checking whether the cart pane is present on webpage
+    $web_driver->wait(10000)->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::className("float-cart__content")));
+    # getting text of the product
+    $product_in_cart = $web_driver->wait(10000)->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::XPath("//*[@id='__next']/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]")))->getText();
+    # Setting the status of test as 'passed' or 'failed' based on the condition; if title of the web page starts with 'BrowserStack'
+    if ($product_on_page == $product_in_cart){
+        $web_driver->executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "Product has been successfully added to the cart!"}}' );
+    }  else {
+        $web_driver->executeScript('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Failed to add product to the cart or some elements might have failed to load."}}');
+    }
+}
+catch(Exception $e){
+    echo 'Message: ' .$e->getMessage();
+}
+$web_driver->quit();
+?> 
